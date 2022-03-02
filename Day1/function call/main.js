e60322636d06cbdab63f230417399ea160e665ad
SamSumit007 <97454993+SamSumit007@users.noreply.github.com>
2022-03-02 21:34:49 +0530

    xsc

---
+++ Day1/function call/main.js
@@ -0,0 +1,9 @@
+const Add = require("./add");
+const Subtract = require("./subtract");
+const Multiply = require("./multiply");
+const Devide = require("./devide");
+
+console.log( Add());
+console.log( Subtract());
+console.log( Multiply());
+console.log( Devide());
---
+++ Day1/function call/subtract.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    var a = 3;
+    var b= 2;
+    var subtract =0;
+    subtract = a-b;
+    console.log('subtract:', subtract)
+  return 'subtract'
+}
+ 
