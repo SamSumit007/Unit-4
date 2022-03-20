1f4c8384e8272360486970e6f2e2f40c36e7d090
SamSumit007 <97454993+SamSumit007@users.noreply.github.com>
2022-03-20 23:33:26 +0530

    day11

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 
-const userControllers = require("./controllers/user.controllers");
+const usersController = require("./controllers/user.controller");
+const postController = require("./controllers/post.controller");
+
+const {register,login} = require("./controllers/auth.controller")
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/users", userControllers);
+app.use("/users", usersController); 
+
+app.post("/register", register)
+
+app.post("/login", login)
+
+ app.use("/post", postController);
+// app.use("/comments", commentsController);
 
 module.exports = app;
