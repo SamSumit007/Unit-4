5197b8865aaeb21b50e0dd84a6ec1d1e6eba4c63
SamSumit007 <97454993+SamSumit007@users.noreply.github.com>
2022-03-24 19:12:29 +0530

    updated

--- Day7/src/controllers/student.controller.js
+++ Day7/src/controllers/student.controller.js
@@ -5,9 +5,9 @@ const Student = require("../models/student.model");
 
 const router = express.Router();
 
-router.get("/students", async (req, res) => {
+router.get("", async (req, res) => {
     try {
-      const students = await Students.find()
+      const students = await Student.find()
         .populate({ path: "student_id", select: "user" })
         .lean()
         .exec();
@@ -18,7 +18,7 @@ router.get("/students", async (req, res) => {
     }
   });
   
-  router.post("/students", async (req, res) => {
+  router.post("", async (req, res) => {
     try {
       const students = await Student.create(req.body);
   
